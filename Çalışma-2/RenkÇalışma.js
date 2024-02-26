82be51b258d66e4ff3067a529afdc529ec80d199
SGR1315 <146672773+SGR1315@users.noreply.github.com>
2024-02-26 22:24:06 +0300

    Add files via upload

---
+++ Çalışma-2/RenkÇalışma.js
@@ -0,0 +1,13 @@
+function ArkaPlanSari(){
+    document.body.style.backgroundColor= "Yellow";
+}
+function ArkaPlanKirmizi(){
+    document.body.style.backgroundColor= "Red";
+}
+function ArkaPlanMavi(){
+    document.body.style.backgroundColor= "Blue";
+}
+function ArkaPlanYeşil(){
+    document.body.style.backgroundColor= "Green";
+}
+
